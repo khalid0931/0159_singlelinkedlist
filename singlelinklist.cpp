0d3965ab523a7520d047dca5f490e46296d9ae14
khalid0931 <mutiarif53@gmail.com>
2026-04-21 11:18:40 +0700

    membuat bool delNode

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -84,4 +84,13 @@ public:
 
         if (!search(nim, previous, current))
             return false;
+        
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+        delete current;
+        return true;
     }
+};
