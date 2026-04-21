24e8aa3c37d64d64cfb16f35faf049bba6c85d03
khalid0931 <mutiarif53@gmail.com>
2026-04-21 11:13:58 +0700

    membuat bool search

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -63,4 +63,25 @@ public:
     {
         return (START == NULL);
     }
-};
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+    }
