4f2511feecf166d364a9b42763072ee24060c7e4
khalid0931 <mutiarif53@gmail.com>
2026-04-21 11:31:29 +0700

    membuat intmain

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -110,6 +110,38 @@ public:
                 cout << currentNode->noMhs << endl;
                 currentNode = currentNode->next;
             }
+            cout << endl;
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
